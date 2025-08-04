65862d11b6f2607d562e204e1ba8130868ab56c9
Faldi Ramadhan <indocrafter76@gmail.com>
2025-08-04 23:54:50 +0700

    merge

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,35 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/{any?}', function ($any = null) {
+    // Jika $any null (akses "/"), fallback ke index.html
+    if (!$any) {
+        return Response::file(public_path('compro/index.html'));
+    }
+
+    $fullPath = public_path("compro/{$any}");
+
+    if (File::exists($fullPath)) {
+        return Response::file($fullPath);
+    }
+
+    $htmlPath = public_path("compro/{$any}.html");
+    if (File::exists($htmlPath)) {
+        return Response::file($htmlPath);
+    }
+
+    $txtPath = public_path("compro/{$any}.txt");
+    if (File::exists($txtPath)) {
+        return response(File::get($txtPath), 200)->header('Content-Type', 'text/plain');
+    }
+
+    // fallback terakhir ke index.html
+    return Response::file(public_path('compro/index.html'));
+})->where('any', '.*');
