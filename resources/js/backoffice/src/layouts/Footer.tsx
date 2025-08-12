9a4d3369c5d7bde98c27f10f6550d773181f2c69
Faldi Ramadhan <indocrafter76@gmail.com>
2025-08-13 00:25:48 +0700

    feat: rpin

--- resources/js/backoffice/src/layouts/Footer.tsx
+++ resources/js/backoffice/src/layouts/Footer.tsx
@@ -7,8 +7,8 @@ const Footer = () => {
 			<footer className="footer h-16 flex items-center px-6 bg-white shadow dark:bg-gray-800 mt-auto">
 				<div className="flex md:justify-between justify-center w-full gap-4">
 					<div>
-						{new Date().getFullYear()} © Zhindaya
-						
+						{new Date().getFullYear()} © Itaki
+
 					</div>
 					<div className="md:flex hidden gap-4 item-center md:justify-end">
 						<Link to="/" className="text-sm leading-5 text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white">
