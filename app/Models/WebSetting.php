c84b7ea2d38f344fbcb2f4b7239492d68e996b59
Armand Iskandarsyah <armand.iskandar96@gmail.com>
2025-08-17 09:26:22 +0700

    feat: add web setting fillable

--- app/Models/WebSetting.php
+++ app/Models/WebSetting.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class WebSetting extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'type',
+        'value'
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
 }
